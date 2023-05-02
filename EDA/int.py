fd2132fd7fbfbccdbf22c5bb3edf9c809ed066ae
AnaBFarinha <anabdfarinha@gmail.com>
2023-05-02 14:14:14 +0100

    missing values taken into account

--- EDA/int.py
+++ EDA/int.py
@@ -1,28 +1,34 @@
 import pandas as pd
+import numpy as np
 
 def float_to_int(df: pd.DataFrame,
                   variables: list[str]
                   ) -> pd.DataFrame:
+    
     """
-    Converts specified columns in a pandas 
-        dataframe from float to integer data type.
+    Converts specified columns in a pandas dataframe 
+        from float to integer data type.
 
     ----------
     Parameters:
      - df (pd.DataFrame): The pandas dataframe 
         to be modified.
-     - variables (list[str]): A list of column 
-        names in the dataframe to be converted.
+     - variables (list[str]): A list of column names
+         in the dataframe to be converted.
 
     ----------
     Returns:
-     - df (pd.DataFrame): The modified pandas 
-        dataframe with specified columns converted 
-        to integer data type.
+     - df (pd.DataFrame): The modified pandas dataframe
+         with specified columns converted to integer data type.
     """
 
     # Iterate through each variable and convert from float to integer
     for i in variables:
+
+        # Check if column contains any missing values
+        if df[i].isnull().values.any():
+            continue
+        
         df[i] = df[i].astype(int)
 
     # Return the modified dataframe
